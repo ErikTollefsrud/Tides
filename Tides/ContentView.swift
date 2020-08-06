02ad47b389049b41f6db667301e1cb9e8db98eb4
Erik Tollefsrud <erik.tollefsrud@gmail.com>
2020-08-06 15:14:08 -0500

    Initial Commit

---
+++ Tides/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tides
+//
+//  Created by Erik Tollefsrud on 8/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tides/TidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidesApp.swift
+//  Tides
+//
+//  Created by Erik Tollefsrud on 8/6/20.
+//
+
+import SwiftUI
+
+@main
+struct TidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
