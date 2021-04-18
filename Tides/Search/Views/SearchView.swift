b5f44cd201b0bc6beb6467fd585c5e42714a2ac8
Klajd Deda <kdeda@id-design.com>
2021-04-18 01:11:47 -0400

    Refactor

--- Tides/Search/Views/SearchView.swift
+++ Tides/Search/Views/SearchView.swift
@@ -4,38 +4,38 @@ import TidesAndCurrentsClient
 import SwiftUI
 
 public struct SearchView: View {
-  let store: Store<SearchState, SearchAction>
-
-  public init(store: Store<SearchState, SearchAction>) {
-    self.store = store
-  }
-
-  public var body: some View {
-    WithViewStore(self.store) { viewStore in
-      VStack {
-        SearchBar(
-          title: "Station Name, City, or State Initial",
-          searchText: viewStore.binding(
-            get: { $0.query },
-            send: { .textChanged($0) }
-          ),
-          isSearching: viewStore.shouldShowActivityIndicator
-        )
-        
-        List(viewStore.filteredItems) { item in
-          
-          Button(action: {print("button tapped on \(item)\nWindows: \(UIApplication.shared.windows[0].isKeyWindow)")}, label: {
-              SearchResultRow(station: item)
-            })
-          
-        }
-        .listStyle(PlainListStyle())
-        .animation(.default)
-        .resignKeyboardOnDragGesture()
-      }
-      .navigationBarTitle("Locations")
+    let store: Store<Search.State, Search.Action>
+    
+    public init(store: Store<Search.State, Search.Action>) {
+        self.store = store
     }
-  }
+    
+    public var body: some View {
+        WithViewStore(self.store) { viewStore in
+            VStack {
+                SearchBar(
+                    title: "Station Name, City, or State Initial",
+                    searchText: viewStore.binding(
+                        get: { $0.query },
+                        send: { .textChanged($0) }
+                    ),
+                    isSearching: viewStore.shouldShowActivityIndicator
+                )
+                
+                List(viewStore.filteredItems) { item in
+                    
+                    Button(action: {print("button tapped on \(item)\nWindows: \(UIApplication.shared.windows[0].isKeyWindow)")}, label: {
+                        SearchResultRow(station: item)
+                    })
+                    
+                }
+                .listStyle(PlainListStyle())
+                .animation(.default)
+                .resignKeyboardOnDragGesture()
+            }
+            .navigationBarTitle("Locations")
+        }
+    }
 }
 
 extension UIApplication {
@@ -65,38 +65,38 @@ struct ResignKeyboardOnDragGesture: ViewModifier {
 // MARK: Previews
 
 struct SearchView_Previews: PreviewProvider {
-  static let searchResult = [
-    Station(
-      id: "12345678",
-      name: "Test 1",
-      state: "MN",
-      latitude: 100.00,
-      longitude: -100.00),
+    static let searchResult = [
+        Station(
+            id: "12345678",
+            name: "Test 1",
+            state: "MN",
+            latitude: 100.00,
+            longitude: -100.00),
+        
+        Station(
+            id: "87654321",
+            name: "Test 2",
+            state: "WI",
+            latitude: 200.00,
+            longitude: -200.00)
+    ]
     
-    Station(
-      id: "87654321",
-      name: "Test 2",
-      state: "WI",
-      latitude: 200.00,
-      longitude: -200.00)
-  ]
-
-  static var previews: some View {
-    let store = Store<SearchState, SearchAction>(
-      initialState: SearchState(
-        query: "Test",
-        items: searchResult,
-        filteredItems: searchResult,
-        shouldShowActivityIndicator: false),
-      reducer: searchReducer,
-      environment: .init(
-        provider: .live,
-        mainQueue: DispatchQueue.main.eraseToAnyScheduler()
-      )
-    )
-    return NavigationView {
-      SearchView(store: store)
+    static var previews: some View {
+        let store = Store<Search.State, Search.Action>(
+            initialState: Search.State(
+                query: "Test",
+                items: searchResult,
+                filteredItems: searchResult,
+                shouldShowActivityIndicator: false),
+            reducer: Search.reducer,
+            environment: .init(
+                provider: .live,
+                mainQueue: DispatchQueue.main.eraseToAnyScheduler()
+            )
+        )
+        return NavigationView {
+            SearchView(store: store)
+        }
+        .environment(\.locale, Locale(identifier: "sv_SE"))
     }
-    .environment(\.locale, Locale(identifier: "sv_SE"))
-  }
 }
--- Tides/TideReading.swift
+++ Tides/TideReading.swift
@@ -9,57 +9,66 @@ import ComposableArchitecture
 import SwiftUI
 import TidesAndCurrentsClient
 
-struct TideReadingState: Equatable {
-    var stationID: String
-    var predictionReading: TidePredictions?
-}
-
-enum TideReadingAction {
-    case onAppear(String)
-    case predictionResponse(Result<TidePredictions?, Never>)
-}
-
-struct TideReadingEnvironment {
-    var tidesAndCurrentProvider: TidesAndCurrentsProvider
-}
-
-let tideReadingReducer = Reducer<TideReadingState, TideReadingAction, TideReadingEnvironment>{ state, action, environment in
-    switch action {
-    case let .onAppear(stationString):
-        return environment.tidesAndCurrentProvider.nextTwoDaysOfPredictions(stationString)
-            .receive(on: DispatchQueue.main)
-            .print()
-            .catchToEffect()
-            .map(TideReadingAction.predictionResponse)
-    case let .predictionResponse(.success(response)):
-        state.predictionReading = response
-        return .none
+struct TideReading {
+    struct State: Equatable {
+        var stationID: String
+        var predictionReading: TidePredictions
+    }
+    
+    enum Action {
+        case onAppear(String)
+        case predictionResponse(Result<TidePredictions, TideError>)
+    }
+    
+    struct Environment {
+        var tidesAndCurrentProvider: TidesAndCurrentsProvider
     }
 }
 
-struct TideReading: View {
+extension TideReading {
+    static let reducer = Reducer<State, Action, Environment>{ state, action, environment in
+        switch action {
+        case let .onAppear(stationString):
+            return environment.tidesAndCurrentProvider.nextTwoDaysOfPredictions(stationString)
+                .receive(on: DispatchQueue.main)
+                .print()
+                .catchToEffect()
+                .map(Action.predictionResponse)
+            
+        case let .predictionResponse(.success(response)):
+            state.predictionReading = response
+            return .none
+            
+        case let .predictionResponse(.failure(error)):
+            state.predictionReading = TidePredictions(predictions: [])
+            return .none
+        }
+    }
+}
+
+struct TideReadingView: View {
     @State var station: Station
-    let store: Store<TideReadingState, TideReadingAction>
+    let store: Store<TideReading.State, TideReading.Action>
     
     var body: some View {
         WithViewStore(store) { viewStore in
             VStack {
-//                Text("Tide Level")
-//                    .font(.title)
-//                Text("Water Level")
-//                    .font(.headline)
-//                HStack {
-//                    Text("Now: ")
-//                    Text("3 ft")
-//                }
-//                .padding()
-//                Text("Tides")
-//                    .font(.headline)
-//                HStack {
-//                    Text("")
-//                }
+                //                Text("Tide Level")
+                //                    .font(.title)
+                //                Text("Water Level")
+                //                    .font(.headline)
+                //                HStack {
+                //                    Text("Now: ")
+                //                    Text("3 ft")
+                //                }
+                //                .padding()
+                //                Text("Tides")
+                //                    .font(.headline)
+                //                HStack {
+                //                    Text("")
+                //                }
                 List {
-                    ForEach(viewStore.predictionReading?.predictions ?? [Tide]()) { prediction in
+                    ForEach(viewStore.predictionReading.predictions) { prediction in
                         Text("\(prediction.time) - \(prediction.value) - \(prediction.type.rawValue)")
                     }
                 }
@@ -72,16 +81,17 @@ struct TideReading: View {
 
 struct TideReading_Previews: PreviewProvider {
     static var previews: some View {
-        TideReading(station: Station.init(
-                        id: "8454000",
-                        name: "Providence",
-                        state: "RI",
-                        latitude: 41.000,
-                        longitude: 21.000),
-                    store: Store(
-                        initialState: TideReadingState(stationID: "8454000"),
-                        reducer: tideReadingReducer,
-                        environment: TideReadingEnvironment(tidesAndCurrentProvider: .live))
+        TideReadingView(
+            station: Station.init(
+                id: "8454000",
+                name: "Providence",
+                state: "RI",
+                latitude: 41.000,
+                longitude: 21.000),
+            store: Store(
+                initialState: TideReading.State(stationID: "8454000", predictionReading: TidePredictions(predictions: [])),
+                reducer: TideReading.reducer,
+                environment: TideReading.Environment(tidesAndCurrentProvider: .live))
         )
     }
 }
